dd2fd573fab788ccae9a40d237341c12c2dea8c7
Pierre-Hugues Husson <phh@phh.me>
2018-02-16 21:41:24 +0100

    Use a phhgsi_xxx device instead of Treble standard ones, to be able to increase system.img

--- generate.sh
+++ generate.sh
@@ -46,7 +46,7 @@ include build/make/target/product/treble_common.mk
 $apps_script
 
 PRODUCT_NAME := $target
-PRODUCT_DEVICE := generic_arm64_$part
+PRODUCT_DEVICE := phhgsi_arm64_$part
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := Phh-Treble $apps_name
 
---
+++ phhgsi_arm64_a/BoardConfig.mk
@@ -0,0 +1,3 @@
+include build/make/target/board/generic_arm64_a/BoardConfig.mk
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
