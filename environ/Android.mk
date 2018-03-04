de92708b123ecfed8be5a41532bd4979437bdaa2
Pierre-Hugues Husson <phh@phh.me>
2018-03-04 22:24:18 +0100

    Create folder before creating rc file

--- environ/Android.mk
+++ environ/Android.mk
@@ -4,6 +4,7 @@ LOCAL_MODULE := treble-environ-rc
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_POST_INSTALL_CMD := \
+	mkdir -p $(PRODUCT_OUT)/system/etc/init/ && \
 	sed -e 's?%BOOTCLASSPATH%?$(PRODUCT_BOOTCLASSPATH)?g' device/phh/treble/environ/init.treble-environ.rc > $(PRODUCT_OUT)/system/etc/init/init.treble-environ.rc && \
 	sed -i -e 's?%SYSTEMSERVERCLASSPATH%?$(PRODUCT_SYSTEM_SERVER_CLASSPATH)?g' $(PRODUCT_OUT)/system/etc/init/init.treble-environ.rc
 
