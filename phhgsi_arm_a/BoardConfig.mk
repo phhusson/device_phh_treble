ccfcad1254389347b997ef4800a526086f36189c
Pierre-Hugues Husson <phh@phh.me>
2018-03-04 19:11:58 +0100

    Set arm32 system partition to 1GB

--- phhgsi_arm_a/BoardConfig.mk
+++ phhgsi_arm_a/BoardConfig.mk
@@ -1,4 +1,5 @@
 include build/make/target/board/generic_arm_a/BoardConfig.mk
 
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
 TARGET_SYSTEM_PROP := device/phh/treble/system.prop $(TARGET_SYSTEM_PROP)
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/phh/treble/bluetooth
