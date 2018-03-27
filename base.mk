76e806921c3ab9489171fbd46d021bdb7bde9816
Pierre-Hugues Husson <phh@phh.me>
2018-03-27 23:39:38 +0200

    Set ro.adb.secure earlier, because vendor might override us

--- base.mk
+++ base.mk
@@ -23,7 +23,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 	ro.build.version.sdk=$(PLATFORM_SDK_VERSION) \
 	ro.build.version.codename=$(PLATFORM_VERSION_CODENAME) \
 	ro.build.version.all_codenames=$(PLATFORM_VERSION_ALL_CODENAMES) \
-	ro.build.version.release=$(PLATFORM_VERSION)
+	ro.build.version.release=$(PLATFORM_VERSION) \
+	ro.adb.secure=0
 
 #VNDK config files
 PRODUCT_COPY_FILES += \
