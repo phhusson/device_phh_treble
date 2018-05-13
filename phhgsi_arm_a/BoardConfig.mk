6b28c3f740058d89c006aaaf9f94896eb09ca11c
Pierre-Hugues Husson <phh@phh.me>
2018-05-14 00:42:36 +0200

    Set lower arm_a size (seen on Koolnee Rainbow)

--- phhgsi_arm_a/BoardConfig.mk
+++ phhgsi_arm_a/BoardConfig.mk
@@ -1,4 +1,4 @@
 include build/make/target/board/generic_arm_a/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
