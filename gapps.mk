0fc7b6135b32fe3ab26a5e10f634d59525feb532
Alexander Pohl <pohl199885@gmail.com>
2018-06-14 18:41:25 +0200

    Point to the real gapps overlay dir

--- gapps.mk
+++ gapps.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
 	device/phh/treble/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2016.xml \
 	device/phh/treble/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2017.xml
 
-DEVICE_PACKAGE_OVERLAYS += device/phh/treble/overlay
+DEVICE_PACKAGE_OVERLAYS += device/phh/treble/overlay-gapps
 GAPPS_VARIANT := nano
 DONT_DEXPREOPT_PREBUILTS := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
