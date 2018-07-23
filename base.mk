425290a94916cc14102b1164852ccc1d2a13b611
Pierre-Hugues Husson <phh@phh.me>
2018-07-23 21:37:40 +0200

    typo

--- base.mk
+++ base.mk
@@ -70,4 +70,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	device/phh/treble/twrp/twrp.rc:system/etc/init/twrp.rc \
 	device/phh/treble/twrp/twrp.sh:system/bin/twrp.sh \
-	device/phh/treble/twrp/buysbox-armv7l:system/bin/busybox_phh
+	device/phh/treble/twrp/busybox-armv7l:system/bin/busybox_phh
