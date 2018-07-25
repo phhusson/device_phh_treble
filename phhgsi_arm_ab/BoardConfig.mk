f446affaf511069fd89aae314ddbcff2521b6dbc
Erfan Abdi <erfangplus@gmail.com>
2018-07-25 20:34:22 +0430

    arm(64)_ab: add bt_firmware dir to root
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- phhgsi_arm_ab/BoardConfig.mk
+++ phhgsi_arm_ab/BoardConfig.mk
@@ -1,4 +1,6 @@
 include build/make/target/board/generic_arm_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
+BOARD_ROOT_EXTRA_FOLDERS += bt_firmware
+
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
