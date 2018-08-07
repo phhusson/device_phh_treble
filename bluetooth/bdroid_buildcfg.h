341d5d9e59f0cfbf32275db7b7b55a655c4c56d2
Pierre-Hugues Husson <phh@phh.me>
2018-08-07 13:09:49 +0200

    P WIP

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -40,12 +40,12 @@ static inline const char* BtmDefLocalName()
     return "hello";
 }
 
-static inline bool BtmBypassExtraAclSetup() {
+static inline int BtmBypassExtraAclSetup() {
 #if 0
     int8_t prop = property_get_bool("persist.sys.bt_acl_bypass", false);
     return prop == true;
 #endif
-    return false;
+    return 1;
 }
 
 #define BTM_DEF_LOCAL_NAME BtmDefLocalName()
