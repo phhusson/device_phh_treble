7495263fbd3131dba904c4968e607cb0f7ba57d4
Maxim <hqdevnews@gmail.com>
2018-09-10 20:11:06 +0700

    Another method of Gapps implementation for Pie
    Opengapps is broken for us, because of certification , we will use prebuilt gapps from other source

--- gapps.mk
+++ gapps.mk
@@ -1,23 +1,5 @@
-PRODUCT_COPY_FILES += \
-	device/phh/treble/empty-permission.xml:system/etc/permissions/com.google.android.camera2.xml \
-	device/phh/treble/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2015.xml \
-	device/phh/treble/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2016.xml \
-	device/phh/treble/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2017.xml
-
-DEVICE_PACKAGE_OVERLAYS += device/phh/treble/overlay-gapps
-GAPPS_VARIANT := nano
-DONT_DEXPREOPT_PREBUILTS := true
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-GAPPS_FORCE_PACKAGE_OVERRIDES := true
-GAPPS_FORCE_BROWSER_OVERRIDES := true
-GAPPS_FORCE_WEBVIEW_OVERRIDES := true
-PRODUCT_PACKAGES += \
-       Chrome \
-       CalculatorGoogle \
-       PrebuiltDeskClockGoogle \
-       CalendarGooglePrebuilt \
-       GoogleHome \
-       LatinImeGoogle \
-       phh-overrides
-
-$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
+ifeq ($(TARGET_ARCH), arm64)
+	TARGET_GAPPS_ARCH := arm64
+else
+	TARGET_GAPPS_ARCH := arm
+endif
