3a68ec19bc5c30cbf471ab2bce7c8d212390ec51
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2018-09-18 16:28:34 +0200

    Declare framework-side hostapd (will be done by treble app)

--- board-base.mk
+++ board-base.mk
@@ -1,3 +1,4 @@
 TARGET_SYSTEM_PROP := device/phh/treble/system.prop $(TARGET_SYSTEM_PROP)
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/phh/treble/bluetooth
 TARGET_EXFAT_DRIVER := exfat
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/phh/treble/framework_manifest.xml
