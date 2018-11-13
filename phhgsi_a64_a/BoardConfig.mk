313c678c189044b9a91a78d16a2762ad514f5979
Pierre-Hugues Husson <phh@phh.me>
2018-11-13 22:25:10 +0100

    Add phhgsi_a64_a for arm32 devices with 64bits binder

---
+++ phhgsi_a64_a/BoardConfig.mk
@@ -0,0 +1,5 @@
+include build/make/target/board/generic_arm_a/BoardConfig.mk
+include device/phh/treble/board-base.mk
+
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
+TARGET_USES_64_BIT_BINDER := true
