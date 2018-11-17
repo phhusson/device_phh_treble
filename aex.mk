10d312ae87620ca010ae3f4b305b82fe17aa10f6
Enes Sastim <sastimenes@gmail.com>
2018-11-17 20:34:52 +0300

    create AEX makefile

---
+++ aex.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/aosp/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
