c2b6687a5a7f6474a71a2e8d261e15209f3e5f24
Pierre-Hugues Husson <phh@phh.me>
2018-11-26 22:49:44 +0100

    Force ro.logd.auditd to true (set to false by Samsung vendor)

--- base.mk
+++ base.mk
@@ -24,7 +24,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 	ro.build.version.all_codenames=$(PLATFORM_VERSION_ALL_CODENAMES) \
 	ro.build.version.release=$(PLATFORM_VERSION) \
 	ro.build.version.security_patch=$(PLATFORM_SECURITY_PATCH) \
-	ro.adb.secure=0 
+	ro.adb.secure=0 \
+	ro.logd.auditd=true
 	
 #Huawei HiSuite (also other OEM custom programs I guess) it's of no use in AOSP builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
