5a9c96ec32372750693ddbb5c414595d39a0de2d
Pierre-Hugues Husson <pierre-hugues.husson@softathome.com>
2019-01-17 15:49:46 +0100

    Be compatible with AOSP master BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE
    In that case, don't enforce system image size
    We will need to implement after build checks to ensure we still work on
    older devices

--- phhgsi_arm_a/BoardConfig.mk
+++ phhgsi_arm_a/BoardConfig.mk
@@ -1,4 +1,6 @@
 include build/make/target/board/generic_arm_a/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
+ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1313583104
+endif
--- phhgsi_arm_ab/BoardConfig.mk
+++ phhgsi_arm_ab/BoardConfig.mk
@@ -1,4 +1,6 @@
 include build/make/target/board/generic_arm_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
+ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+endif
