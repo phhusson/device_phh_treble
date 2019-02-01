69f230df9cd48308bfac6346f9b549501733a52b
Sᴀᴍᴀʀ Vɪꜱᴘᴜᴛᴇ <samarvispute121@gmail.com>
2019-02-01 15:47:27 +0530

    Fix low brightness issue on realme 2 pro

--- rw-system.sh
+++ rw-system.sh
@@ -163,6 +163,10 @@ if getprop ro.vendor.build.fingerprint | grep -q -i \
     setprop persist.sys.qcom-brightness "$(cat /sys/class/leds/lcd-backlight/max_brightness)"
 fi
 
+if getprop ro.vendor.product.device |grep -iq -e RMX1801 -e RMX1803 -e RMX1807;then	
+    setprop persist.sys.qcom-brightness $(cat /sys/class/leds/lcd-backlight/max_brightness)
+fi
+
 if getprop ro.vendor.build.fingerprint | grep -iq \
     -e Xiaomi/beryllium/beryllium -e Xiaomi/sirius/sirius \
     -e Xiaomi/dipper/dipper -e Xiaomi/ursa/ursa -e Xiaomi/polaris/polaris \
