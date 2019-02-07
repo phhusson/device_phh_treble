afe92a339a32cba4cdc4816f968e9ee5e813cf5f
Hackintosh 5 <penn.mackintosh@gmail.com>
2019-02-07 09:43:39 +0000

    Fix indentation in phh-on-boot.sh

--- phh-on-boot.sh
+++ phh-on-boot.sh
@@ -2,21 +2,21 @@
 
 [ "$(getprop vold.decrypt)" == "trigger_restart_min_framework" ] && exit 0
 if [ -f /vendor/bin/mtkmal ];then
-    if [ "$(getprop persist.mtk_ims_support)" == 1 -o "$(getprop persist.mtk_epdg_support)" == 1 ];then
-        setprop persist.mtk_ims_support 0
-        setprop persist.mtk_epdg_support 0
-        reboot
+	if [ "$(getprop persist.mtk_ims_support)" == 1 -o "$(getprop persist.mtk_epdg_support)" == 1 ];then
+		setprop persist.mtk_ims_support 0
+		setprop persist.mtk_epdg_support 0
+		reboot
     fi
 fi
 
 #Clear looping services
 sleep 30
 getprop | \
-    grep restarting | \
-    sed -nE -e 's/\[([^]]*).*/\1/g'  -e 's/init.svc.(.*)/\1/p' |
-    while read svc ;do
-        setprop ctl.stop $svc
-    done
+	grep restarting | \
+	sed -nE -e 's/\[([^]]*).*/\1/g'  -e 's/init.svc.(.*)/\1/p' |
+	while read svc ;do
+	setprop ctl.stop $svc
+	done
 
 if grep -qF android.hardware.boot /vendor/manifest.xml;then
 	bootctl mark-boot-successful
