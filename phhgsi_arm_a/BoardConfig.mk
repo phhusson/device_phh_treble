4f40b50e9f88c737bd68119f8b9d57059ec4bd5a
Brian Wang <wangpy@cloudmosa.com>
2019-04-18 08:39:35 +0000

    Decrease system partition size for arm_a images so it fits in for more devices

--- phhgsi_arm_a/BoardConfig.mk
+++ phhgsi_arm_a/BoardConfig.mk
@@ -2,5 +2,5 @@ include build/make/target/board/generic_arm_a/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1313583104
 endif
