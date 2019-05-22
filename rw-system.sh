47062277a4fcf6bb0f6f5efafa12f48100ae1ac6
Pierre-Hugues Husson <phh@phh.me>
2019-05-22 20:43:18 +0200

    Add cereus in the no sound effects list
    https://forum.xda-developers.com/showpost.php?p=79568310&postcount=1998

--- rw-system.sh
+++ rw-system.sh
@@ -174,7 +174,7 @@ if getprop ro.vendor.build.fingerprint | grep -iq \
     -e motorola/ali/ali -e iaomi/perseus/perseus -e iaomi/platina/platina \
     -e iaomi/equuleus/equuleus -e motorola/nora -e xiaomi/nitrogen \
     -e motorola/hannah -e motorola/james -e motorola/pettyl -e iaomi/cepheus \
-    -e iaomi/grus;then
+    -e iaomi/grus -e xiaomi/cereus;then
     mount -o bind /mnt/phh/empty_dir /vendor/lib64/soundfx
     mount -o bind /mnt/phh/empty_dir /vendor/lib/soundfx
     setprop  ro.audio.ignore_effects true
