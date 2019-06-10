1a2c9165c8c2cd85f2b54d4a8d0d4af14fbe654e
Pierre-Hugues Husson <phh@phh.me>
2019-06-10 10:42:05 +0200

    Update phhgsi_arm64_a system.img size to match Huawei P9
    Fixes https://github.com/phhusson/treble_experimentations/issues/546

--- phhgsi_arm64_a/BoardConfig.mk
+++ phhgsi_arm64_a/BoardConfig.mk
@@ -2,5 +2,5 @@ include build/make/target/board/generic_arm64_a/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2080374784
 endif
