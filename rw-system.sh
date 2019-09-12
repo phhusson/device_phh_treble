0fff673bd37904e3b8609fd60daaa81000279d06
Pierre-Hugues Husson <phh@phh.me>
2019-09-12 20:57:07 +0200

    also override 64b keymaster3device

--- rw-system.sh
+++ rw-system.sh
@@ -31,7 +31,7 @@ fixSPL() {
             /system/lib64/vndk/libsoftkeymasterdevice.so /system/lib/vndk/libsoftkeymasterdevice.so \
             /system/lib/vndk-26/libsoftkeymasterdevice.so \
             /system/lib/vndk-27/libsoftkeymasterdevice.so /system/lib64/vndk-27/libsoftkeymasterdevice.so \
-	    /vendor/lib/libkeymaster3device.so ; do
+	    /vendor/lib/libkeymaster3device.so /vendor/lib64/libkeymaster3device.so ; do
             [ ! -f "$f" ] && continue
             # shellcheck disable=SC2010
             ctxt="$(ls -lZ "$f" | grep -oE 'u:object_r:[^:]*:s0')"
