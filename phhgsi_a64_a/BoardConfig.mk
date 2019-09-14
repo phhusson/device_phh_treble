be34ac239b2430113d9322a496f6752f62de0cee
Pierre-Hugues Husson <phh@phh.me>
2019-09-14 21:17:03 +0200

    Fixed build for a-only (now called System-as-System)

--- phhgsi_a64_a/BoardConfig.mk
+++ phhgsi_a64_a/BoardConfig.mk
@@ -1,7 +1,8 @@
-include build/make/target/board/generic_arm_a/BoardConfig.mk
+include build/make/target/board/generic_arm_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
 endif
 TARGET_USES_64_BIT_BINDER := true
+BOARD_SYSTEMIMAGE_AS_SYSTEM := true
--- phhgsi_arm_a/BoardConfig.mk
+++ phhgsi_arm_a/BoardConfig.mk
@@ -1,4 +1,4 @@
-include build/make/target/board/generic_arm_a/BoardConfig.mk
+include build/make/target/board/generic_arm_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
@@ -6,3 +6,4 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1313583104
 endif
 
 TARGET_USES_64_BIT_BINDER := false
+BOARD_SYSTEMIMAGE_AS_SYSTEM := true
