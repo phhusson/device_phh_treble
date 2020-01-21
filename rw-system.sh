da0775f9ee79f75fed80ee61f0b5b411618619fb
Mehran Kholdi <semekh@hamravesh.com>
2020-01-21 22:06:59 +0330

    Add Redmi 7 - onc(lite) to fingerprint input events blacklist

--- rw-system.sh
+++ rw-system.sh
@@ -86,7 +86,7 @@ changeKeylayout() {
         -e xiaomi/wayne -e xiaomi/jasmine -e xiaomi/jasmine_sprout \
         -e xiaomi/platina -e iaomi/perseus -e xiaomi/ysl \
         -e xiaomi/nitrogen -e xiaomi/daisy -e xiaomi/sakura \
-        -e xiaomi/whyred -e xiaomi/tulip; then
+        -e xiaomi/whyred -e xiaomi/tulip -e xiaomi/onc; then
         if [ ! -f /mnt/phh/keylayout/uinput-goodix.kl ]; then
           cp /system/phh/empty /mnt/phh/keylayout/uinput-goodix.kl
           chmod 0644 /mnt/phh/keylayout/uinput-goodix.kl
