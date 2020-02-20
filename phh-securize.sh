6a7718a60ba5825cc394bab6cfd5691eec69bcf9
Myst33d <myst33d@gmail.com>
2020-02-20 19:38:35 +0600

    Properly delete superuser app

--- phh-securize.sh
+++ phh-securize.sh
@@ -9,7 +9,7 @@ rm /system/xbin/su
 rm /system/bin/phh-su
 rm /system/etc/init/su.rc
 rm /system/bin/phh-securize.sh
-rm -Rf /system/bin/me.phh.superuser/
+rm -Rf /system/app/me.phh.superuser/
 mount -o remount,ro /
 mount -o remount,ro /system
 sync
