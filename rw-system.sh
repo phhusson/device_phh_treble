3d3450c5daf67fbd93b4c37b9d0fa116951c7c74
Naveen Kumar <ynaveenk.0513@gmail.com>
2020-02-29 21:33:28 +0530

    Adding Brightness and DT2W fix to CPH1859 and CPH1961

--- rw-system.sh
+++ rw-system.sh
@@ -241,6 +241,10 @@ if getprop ro.vendor.product.device |grep -iq -e RMX1801 -e RMX1803 -e RMX1807 -
     setprop persist.sys.qcom-brightness "$(cat /sys/class/leds/lcd-backlight/max_brightness)"
 fi
 
+if getprop ro.build.overlay.deviceid |grep -q -e CPH1859 -e CPH1861;then	
+    setprop persist.sys.qcom-brightness "$(cat /sys/class/leds/lcd-backlight/max_brightness)"
+fi
+
 if getprop ro.vendor.build.fingerprint | grep -iq \
     -e Xiaomi/beryllium/beryllium -e Xiaomi/sirius/sirius \
     -e Xiaomi/dipper/dipper -e Xiaomi/ursa/ursa -e Xiaomi/polaris/polaris \
