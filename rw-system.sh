2476ef278671581f8935add01e4ed0fcd323621d
Pierre-Hugues Husson <phh@phh.me>
2020-03-26 11:45:44 +0100

    Yet another broken sw codec2

--- rw-system.sh
+++ rw-system.sh
@@ -655,6 +655,6 @@ if getprop ro.build.overlay.deviceid |grep -qE '^RMX';then
     fi
 fi
 
-if [ "$vndk" -le 28 ] && getprop ro.hardware |grep -q -e mt6761 -e mt6763;then
+if [ "$vndk" -le 28 ] && getprop ro.hardware |grep -q -e mt6761 -e mt6763 -e mt6765;then
     setprop debug.stagefright.ccodec 0
 fi
