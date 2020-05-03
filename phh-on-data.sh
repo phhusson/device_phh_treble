23cd1f93b6259cf0ce26eb105db3207c6b4c6f37
Pierre-Hugues Husson <phh@phh.me>
2020-05-03 18:01:26 +0200

    Add property to use CAF vendor media_profiles
    This is controlled through persist.sys.phh.caf.media_profile
    system_server loads this file, so it requires a reboot to apply

--- phh-on-data.sh
+++ phh-on-data.sh
@@ -5,3 +5,7 @@ if getprop persist.sys.phh.no_vendor_overlay |grep -q true;then
 		mount /mnt/phh/empty_dir/ /$part/overlay
 	done
 fi
+
+if getprop persist.sys.phh.caf.media_profile |grep -q true;then
+    setprop media.settings.xml "/vendor/etc/media_profiles_vendor.xml"
+fi
