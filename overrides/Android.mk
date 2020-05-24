0781a41c00524747c72ff12f01c7ba922ebec83f
Pierre-Hugues Husson <phh@phh.me>
2020-05-24 20:12:23 +0200

    Disable GMS demo SetupWizard integration because it is broken
    It references com.android.setupwizard.FLOW_CHOICE, which is no longer
    supported by the SetupWizard

--- overrides/Android.mk
+++ overrides/Android.mk
@@ -30,6 +30,24 @@ PACKAGES.$(LOCAL_MODULE).OVERRIDES := \
 	Maps \
 	Gmail2 \
 	GoogleContacts \
+	GmsSampleIntegration \
+	GmsSampleIntegrationGo \
+	GmsSampleIntegrationGo2GB \
+	GmsSampleIntegrationGo512M \
+	GmsEEAType1Integration \
+	GmsEEAType1IntegrationGo \
+	GmsEEAType2Integration \
+	GmsEEAType2IntegrationGo \
+	GmsEEAType3aIntegration \
+	GmsEEAType3aIntegrationGo \
+	GmsEEAType3bIntegration \
+	GmsEEAType3bIntegrationGo \
+	GmsEEAType4aIntegration \
+	GmsEEAType4aIntegrationGo \
+	GmsEEAType4bIntegration \
+	GmsEEAType4bIntegrationGo \
+	GmsEEAType4cIntegration \
+	GmsEEAType4cIntegrationGo
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
