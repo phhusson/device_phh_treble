208a7895de65766b88b80d0321f85884bda3e6f6
Pierre-Hugues Husson <phh@phh.me>
2020-05-26 14:14:16 +0200

    Add talkback (space has been freed by removing FilesGoogle)

--- gapps.mk
+++ gapps.mk
@@ -10,6 +10,7 @@ PRODUCT_PACKAGES += \
 	phh-gapps-overrides \
 	Chrome \
 	GoogleContactsSyncAdapter \
+	talkback \
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES := $(filter-out ro.boot.vendor.overlay.theme=%,$(PRODUCT_SYSTEM_DEFAULT_PROPERTIES))
 endif
