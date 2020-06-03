65af98b51830f5fa1ef0f0eb927c34f5018a5303
Pierre-Hugues Husson <phh@phh.me>
2020-06-03 20:21:50 +0200

    Detect looping mediacodec earlier

--- phh-on-data.sh
+++ phh-on-data.sh
@@ -9,3 +9,8 @@ fi
 if getprop persist.sys.phh.caf.media_profile |grep -q true;then
     setprop media.settings.xml "/vendor/etc/media_profiles_vendor.xml"
 fi
+
+if [ "$vndk" = 27 ] && getprop init.svc.mediacodec |grep -q restarting;then
+    mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
+    mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
+fi
