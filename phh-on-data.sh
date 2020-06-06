dcca8cba17f7ee6301939f37837348d082e5542e
Pierre-Hugues Husson <phh@phh.me>
2020-06-06 19:32:38 +0200

    Some Pie vendor are also affected by minijail issue

--- phh-on-data.sh
+++ phh-on-data.sh
@@ -14,3 +14,10 @@ if [ "$vndk" = 27 ] && getprop init.svc.mediacodec |grep -q restarting;then
     mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
     mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
 fi
+
+if [ "$vndk" = 28 ] && getprop |grep init.svc | grep media |grep -q restarting;then
+    mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
+    mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
+    mount /system/lib64/vndk-27/libminijail.so /system/lib64/vndk-28/libminijail.so
+    mount /system/lib/vndk-27/libminijail.so /system/lib/vndk-28/libminijail.so
+fi
