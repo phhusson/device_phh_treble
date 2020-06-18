68458906901f586d2d3585bfcc905e8b15fda8f2
Pierre-Hugues Husson <phh@phh.me>
2020-06-18 20:24:21 +0200

    Try to fix a race condition where init.svc could /not/ be restarting at the time of checking

--- phh-on-boot.sh
+++ phh-on-boot.sh
@@ -35,12 +35,13 @@ fi
 setprop ctl.stop storageproxyd
 
 sleep 10
-if [ "$vndk" = 27 ] && getprop init.svc.mediacodec |grep -q restarting;then
+crashingProcess=$(getprop ro.init.updatable_crashing_process_name |grep media)
+if [ "$vndk" = 27 ] && ( getprop init.svc.mediacodec |grep -q restarting || [ -n "$crashingProcess" ]);then
     mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
     mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
 fi
 
-if [ "$vndk" = 28 ] && getprop |grep init.svc | grep media |grep -q restarting;then
+if [ "$vndk" = 28 ] && ( getprop |grep init.svc | grep media |grep -q restarting || [ -n "$crashingProcess" ] );then
     mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
     mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
     mount /system/lib64/vndk-27/libminijail.so /system/lib64/vndk-28/libminijail.so
