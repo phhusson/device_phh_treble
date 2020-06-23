4b2f105e778d349ea8f3de553232710ad6e18082
Pierre-Hugues Husson <phh@phh.me>
2020-06-23 21:40:55 +0200

    Yet another c2 broken device

--- rw-system.sh
+++ rw-system.sh
@@ -709,7 +709,7 @@ if getprop ro.build.overlay.deviceid |grep -qE '^RMX';then
     fi
 fi
 
-if [ "$vndk" -le 28 ] && getprop ro.hardware |grep -q -e mt6761 -e mt6763 -e mt6765 -e mt6785 -e mt8768 -e mt6779 -e mt6771;then
+if [ "$vndk" -le 28 ] && getprop ro.hardware |grep -q -e mt6761 -e mt6763 -e mt6765 -e mt6785 -e mt8768 -e mt6779 -e mt6771 -e mt8766;then
     setprop debug.stagefright.ccodec 0
 fi
 
