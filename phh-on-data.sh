c2a0997836617be4c4a87c49b3ed3a4ff020b8be
darkestDark <36689074+darkestDark@users.noreply.github.com>
2020-08-05 20:04:01 +0200

    libminijail.so error
    check for vndk only to mount

--- phh-on-data.sh
+++ phh-on-data.sh
@@ -13,13 +13,13 @@ if getprop persist.sys.phh.caf.media_profile |grep -q true;then
     setprop media.settings.xml "/vendor/etc/media_profiles_vendor.xml"
 fi
 
-crashingProcess=$(getprop ro.init.updatable_crashing_process_name |grep media)
-if [ "$vndk" = 27 ] && ( getprop init.svc.mediacodec |grep -q restarting || [ -n "$crashingProcess" ] );then
+
+if [ "$vndk" = 27 ];then
     mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
     mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
 fi
 
-if [ "$vndk" = 28 ] && ( getprop |grep init.svc | grep media |grep -q restarting || [ -n "$crashingProcess" ] );then
+if [ "$vndk" = 28 ];then
     mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
     mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
     mount /system/lib64/vndk-27/libminijail.so /system/lib64/vndk-28/libminijail.so
