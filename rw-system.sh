b7b1bcfb38ce1c40a42654783ba1a0107be92177
pjgowtham <pjgowtham@gmail.com>
2020-08-18 19:55:14 +0530

    RMX2001 : Set default LCD density to 440
    * Stock ROM uses 480 but it looks too small. So we use 440 instead and that looks perfect.

--- rw-system.sh
+++ rw-system.sh
@@ -309,6 +309,7 @@ fi
 if getprop ro.vendor.product.device |grep -iq -e RMX2001;then
     setprop persist.sys.phh.fingerprint.nocleanup true
     setprop persist.sys.qcom-brightness "$(cat /sys/class/leds/lcd-backlight/max_brightness)"
+    setprop ro.sf.lcd_density "440"
 fi
 
 if getprop ro.vendor.product.device |grep -iq -e RMX1801 -e RMX1803 -e RMX1807;then
