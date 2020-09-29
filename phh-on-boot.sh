41f0817f3fab4361216c5e3bce3660c5045f665b
Pierre-Hugues Husson <phh@phh.me>
2020-09-29 10:45:45 +0200

    Up vendor minijail override, using vndk v28 minijail

--- phh-on-boot.sh
+++ phh-on-boot.sh
@@ -36,18 +36,20 @@ setprop ctl.stop storageproxyd
 
 sleep 10
 
+minijailSrc=/system/system_ext/apex/com.android.vndk.v28/lib/libminijail.so
+minijailSrc64=/system/system_ext/apex/com.android.vndk.v28/lib64/libminijail.so
 if [ "$vndk" = 27 ];then
-    mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
-    mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
+    mount $minijailSrc64 /vendor/lib64/libminijail_vendor.so
+    mount $minijailSrc /vendor/lib/libminijail_vendor.so
 fi
 
 if [ "$vndk" = 28 ];then
-    mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail_vendor.so
-    mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail_vendor.so
-    mount /system/lib64/vndk-27/libminijail.so /system/lib64/vndk-28/libminijail.so
-    mount /system/lib/vndk-27/libminijail.so /system/lib/vndk-28/libminijail.so
-    mount /system/lib64/vndk-27/libminijail.so /vendor/lib64/libminijail.so
-    mount /system/lib/vndk-27/libminijail.so /vendor/lib/libminijail.so
+    mount $minijailSrc64 /vendor/lib64/libminijail_vendor.so
+    mount $minijailSrc /vendor/lib/libminijail_vendor.so
+    mount $minijailSrc64 /system/lib64/vndk-28/libminijail.so
+    mount $minijailSrc /system/lib/vndk-28/libminijail.so
+    mount $minijailSrc64 /vendor/lib64/libminijail.so
+    mount $minijailSrc /vendor/lib/libminijail.so
 fi
 
 #Clear looping services
