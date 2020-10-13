1be7bd1bb147e69f88049aed4c440bb204a7eca4
Pierre-Hugues Husson <phh@phh.me>
2020-10-13 17:19:48 +0200

    Fix CAF audio policy on non-boot

--- phh-prop-handler.sh
+++ phh-prop-handler.sh
@@ -121,6 +121,7 @@ if [ "$1" == "persist.sys.phh.caf.audio_policy" ];then
 
     if [[ "$prop_value" == 1 ]];then
         umount /vendor/etc/audio
+        umount /vendor/etc/audio
         mount /vendor/etc/audio/audio_policy_configuration.xml /vendor/etc/audio_policy_configuration.xml
     else
         umount /vendor/etc/audio_policy_configuration.xml
