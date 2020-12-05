30071d042053b67e4ec6d7332ae187d6cd4542db
Pierre-Hugues Husson <phh@phh.me>
2020-12-05 22:21:06 +0100

    Use ext4 share duplicate blocks, it is sadly needed to attain acceptable partition size, because of duplication made by apexes

--- board-base.mk
+++ board-base.mk
@@ -4,4 +4,3 @@ TARGET_EXFAT_DRIVER := exfat
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/phh/treble/framework_manifest.xml
 
 BOARD_ROOT_EXTRA_FOLDERS += bt_firmware sec_storage efs
-BOARD_EXT4_SHARE_DUP_BLOCKS := false
