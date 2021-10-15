c09b32e27bf95a468b61d98226d02f76b4ebd025
Pierre-Hugues Husson <phh@phh.me>
2021-10-15 13:01:10 -0400

    Include /persist sepolicy and /dsp and /firmware symlink on a64 build like generic_arm64 does

--- phhgsi_a64_ab/BoardConfig.mk
+++ phhgsi_a64_ab/BoardConfig.mk
@@ -1,6 +1,11 @@
 include build/make/target/board/generic/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
+# Copy pasted from build/make/target/board/generic_arm64/BoardConfig.mk
+BOARD_ROOT_EXTRA_SYMLINKS += /vendor/lib/dsp:/dsp
+BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
+BOARD_SEPOLICY_DIRS += build/make/target/board/generic_arm64/sepolicy
+
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
 endif
