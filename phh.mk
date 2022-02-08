b3aad18611afa212db41af2249380992fa7da497
Pierre-Hugues Husson <phh@phh.me>
2022-02-08 15:38:13 -0500

    Goodbye christmas animation. Put MY OTA json in my own specific file

--- phh.mk
+++ phh.mk
@@ -1,3 +1,3 @@
-PRODUCT_COPY_FILES += \
-	device/phh/treble/bootanimation.zip:system/product/media/bootanimation.zip \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	ro.system.ota.json_url=https://raw.githubusercontent.com/phhusson/treble_experimentations/master/ota/squeak/ota.json
 
