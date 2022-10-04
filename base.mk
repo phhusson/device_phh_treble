948497d23a073c298913206b6970dc9651bc211d
Pierre-Hugues Husson <phh@phh.me>
2022-10-04 18:06:30 -0400

    Revert "base: Provide libnfc-nci"
    This reverts commit 323ef8d9159ee9876f29a474815bfc4f100a19d6.
    
    Android already imports its own libnfc-nci.conf

--- base.mk
+++ base.mk
@@ -59,7 +59,6 @@ PRODUCT_COPY_FILES += \
 #   Provide default libnfc-nci.conf file for devices that does not have one in
 #   vendor/etc
 PRODUCT_COPY_FILES += \
-	system/nfc/conf/libnfc-nci.conf:system/etc/libnfc-nci.conf \
 	device/phh/treble/nfc/libnfc-nci.conf:system/phh/libnfc-nci-oreo.conf \
 	device/phh/treble/nfc/libnfc-nci-huawei.conf:system/phh/libnfc-nci-huawei.conf
 
