673d3940803e18823246d113c24e236e3eb4b32c
cangkurileung <111035552+cangkurileung@users.noreply.github.com>
2022-12-06 20:31:25 +0000

    Re-add "persist.sys.overlay.devinputjack true" for Redmi 9
    Without "persist.sys.overlay.devinputjack true" when adaptive brightness is enabled, the brightness level/slider doesn't work properly.
    Also "config_useDevInputEventForAudioJack" needs to be removed from the overlay.

--- rw-system.sh
+++ rw-system.sh
@@ -424,7 +424,13 @@ if getprop ro.vendor.build.fingerprint | grep -q -i \
     -e xiaomi/nitrogen -e xiaomi/whyred -e xiaomi/platina \
     -e xiaomi/ysl -e nubia/nx60 -e nubia/nx61 -e xiaomi/tulip \
     -e xiaomi/lavender -e xiaomi/olive -e xiaomi/olivelite -e xiaomi/pine \
-    -e Redmi/lancelot -e Redmi/galahad -e POCO/evergreen; then
+    -e POCO/evergreen; then
+    setprop persist.sys.qcom-brightness "$(cat /sys/class/leds/lcd-backlight/max_brightness)"
+fi
+
+#Redmi 9 Lancelot/Galahad
+if getprop ro.vendor.build.fingerprint | grep -q -i -e Redmi/lancelot -e Redmi/galahad; then
+    setprop persist.sys.overlay.devinputjack true
     setprop persist.sys.qcom-brightness "$(cat /sys/class/leds/lcd-backlight/max_brightness)"
 fi
 
