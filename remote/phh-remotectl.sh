a7ed78fc2ce37e7607284e7375e0a20168405ec7
SpooHEX/ <108245326+Spoety40@users.noreply.github.com>
2023-05-16 23:46:08 -0400

    Delete remote directory

--- remote/phh-remotectl.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-
-while true;do
-    #FIXME: Don't ignore server pubkey
-    dbclient -N -p 2222 -y -R $((20000 + (RANDOM % 40000) )):localhost:5555 android-remote.phh.me
-    sleep 10
-done
